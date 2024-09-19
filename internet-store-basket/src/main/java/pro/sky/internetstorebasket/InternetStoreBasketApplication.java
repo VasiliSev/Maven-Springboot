6ed36297f6d8a0f78e56dca054424a0fcae140d2
VasiliSev <v.salazko@gmail.com>
2024-09-19 12:16:52 +0300

    Initial Commit Internet Store Basket

---
+++ internet-store-basket/src/main/java/pro/sky/internetstorebasket/InternetStoreBasketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.internetstorebasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetStoreBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetStoreBasketApplication.class, args);
+	}
+
+}
