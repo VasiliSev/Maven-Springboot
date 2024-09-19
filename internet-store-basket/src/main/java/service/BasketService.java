7c3c8449aece4f0768241b4b09b5825a3e54f3e3
VasiliSev <v.salazko@gmail.com>
2024-09-19 14:03:14 +0300

    Internet Store Basket

---
+++ internet-store-basket/src/main/java/service/BasketService.java
@@ -0,0 +1,41 @@
+package pro.sky.internetstorebasket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.*;
+
+@Service
+public class BasketService {
+
+    private final Basket basket;
+
+    public BasketService(Basket basket) {
+        this.basket = basket;
+    }
+
+    public void add(List<Integer> ids) {
+        basket.addItems(ids);
+    }
+
+    public Collection<Integer> get() {
+        return basket.getItems();
+    }
+
+    @Component
+    @SessionScope
+    public static class Basket {
+
+        private final Set<Integer> items = new HashSet<>();
+
+        public void addItems(List<Integer> ids) {
+            items.addAll(ids);
+        }
+
+        public Collection<Integer> getItems() {
+            return Collections.unmodifiableSet(items);
+        }
+
+    }
+}
