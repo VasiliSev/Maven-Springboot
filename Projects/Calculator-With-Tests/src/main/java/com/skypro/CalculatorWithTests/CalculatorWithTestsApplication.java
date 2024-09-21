aae7f0dee251a7e6bdb2e533ac133b3398a51dd2
VasiliSev <v.salazko@gmail.com>
2024-09-21 12:14:34 +0300

    Init Commit Calculator

---
+++ Projects/Calculator-With-Tests/src/main/java/com/skypro/CalculatorWithTests/CalculatorWithTestsApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.CalculatorWithTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWithTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWithTestsApplication.class, args);
+	}
+
+}
